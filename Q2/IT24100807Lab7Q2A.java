6c616678d5e36aa119a3d09f9c5f9023a9fc9d3c
IT24100807 <IT24100807@my.sliit.lk>
2024-09-23 12:46:14 +0530

    Add files via upload

---
+++ Q2/IT24100807Lab7Q2A.java
@@ -0,0 +1,9 @@
+public class IT24100807Lab7Q2A {
+  public static void main (String[] args) {
+    String des="$ $ $ $ $";
+    int line = 4;
+    for(int i=1;i<=line;i++) {
+      System.out.println(des);
+    }
+  }
+}
